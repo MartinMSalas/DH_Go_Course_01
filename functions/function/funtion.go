efee26d83ad63ebaad863b114ed0187968233621
MartinMSalas <ingmartinmsalas@gmail.com>
2024-02-04 01:04:11 -0300

    feat: add FactoryOp

--- functions/function/funtion.go
+++ functions/function/funtion.go
@@ -1,8 +1,91 @@
 package function
+
 import (
+	"errors"
 	"fmt"
 )
+
+type Operation int
+
+const (
+	SUM Operation = iota
+	SUB
+	DIV
+	MUL
+)
+
 func Display(myValue int) {
+	display(myValue)
+}
+
+func display(myValue int) {
 	fmt.Println("Hello, World!")
 	fmt.Printf("My integer variable is %d , his type is %T, his addres is %v \n\n", myValue, myValue, &myValue)
+}
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func RepeatString(increment int, value string) {
+	for i := 0; i < increment; i++ {
+		fmt.Println(value)
+	}
+}
+
+func Calc(op Operation, x, y float64) (float64, error) {
+	switch op {
+	case SUM:
+		return x + y, nil
+	case SUB:
+		return x - y, nil
+	case DIV:
+		if y == 0 {
+			return 0, errors.New("division by zero")
+		}
+		return x / y, nil
+	case MUL:
+		return x * y, nil
+	default:
+		return 0, errors.New("invalid operation")
+	}
+
+}
+
+func Split(v int) (x, y int) {
+	x = v * 4 / 9
+	y = v - x
+	return
+}
+
+func MSum(values ...float64) (result float64) {
+	for _, value := range values {
+		result += value
+	}
+	return
+}
+
+func MOperations(op Operation, values ...float64) (result float64, err error) {
+	if len(values) == 0 {
+		return 0, errors.New("no values to operate")
+	}
+	result = values[0]
+	for _, value := range values[1:] {
+		switch op {
+		case SUM:
+			result += value
+		case SUB:
+			result -= value
+		case MUL:
+			result *= value
+		case DIV:
+			if value == 0 {
+				return 0, errors.New("division by zero")
+			}
+			result /= value
+		default:
+			return 0, errors.New("invalid operation")
+		}
+	}
+	return
 }
--- functions/main.go
+++ functions/main.go
@@ -1,13 +1,41 @@
 package main
+
 import (
-	
 	"DH_Go_Course_01/functions/function"
+	"fmt"
 )
 
 func main() {
-	myIntVar := 10
+	//myIntVar := 10
 
 	
-	function.Display(myIntVar)
+	//function.Display(myIntVar)
+	//function.Display(function.Add(5, 6))
+	//function.RepeatString(5, "Hello, World!")
+	result, err := function.Calc(function.DIV, 50, 10)
+	if err != nil {
+		fmt.Println("An error occurred: ",err)
+		
+		
+		return
+	}
+	fmt.Println("The result is: ", result)
+
+	x,y := function.Split(10)
+	fmt.Println("The result of the split is: ", x, y)
+
+	fmt.Println("The result of the sum is: ", function.MSum(1,2,3,4,5))
+
+	result,err = function.MOperations(function.SUM, 1,2,3,4,5)
+	if err != nil {
+		fmt.Println("An error occurred: ", err)
+		return
+	}
+	fmt.Println("The result of the operations is: ", result)
+
+	factOpFunc := function.FactoryOperation(function.SUM)
+	fmt.Println("The result of the factory operation is: ", factOpFunc(5, 6))
+
 }
+
 
